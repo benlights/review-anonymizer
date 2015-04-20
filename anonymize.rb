1fdab2229176e657a238276856b5892067464c3d
Matthew Bunday <zenceph@gmail.com>
2015-04-20 15:48:23 -0400

    Finished script

--- anonymize.rb
+++ anonymize.rb
@@ -1,16 +1,23 @@
 invoke_path = Dir.pwd
+timestamp = Time.now.strftime('%F')
 
-review_folder = File.join(invoke_path, "review_#{Time.now.strftime('%F')}")
-
+review_folder = File.join(invoke_path, "review_#{timestamp}")
 `mkdir #{review_folder}`
 
 Dir.chdir(ARGV[0])
 
-foreign_branches = `git branch --list -r`
+key_file = File.join(invoke_path, "review_key_#{timestamp}.txt")
+File.open(key_file, 'w') do |f|
 
-# foreign_branches.split("\n").each_with_index do |branch, index|
-#   branch_folder = File.join(review_folder, index.to_s)
-#   `mkdir branch_folder`
-#   p branch.strip
-#   # `git checkout #{branch}`
-# end
+  foreign_branches = `git branch --list -r`
+  foreign_branches.split("\n").each_with_index do |branch, index|
+    puts "Fetching branch #{index}"
+    f.puts "#{index}: #{branch}"
+
+    branch_folder = File.join(review_folder, index.to_s)
+    `mkdir #{branch_folder}`
+
+    `git checkout #{branch} 2>&1`
+    `cp -r * #{branch_folder}`
+  end
+end
