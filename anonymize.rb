bb3da819f1918c88e97aea58ecb8676aac5de48c
Matthew Bunday <zenceph@gmail.com>
2015-04-20 15:35:41 -0400

    Correct filename

--- anonymize.rb
+++ anonymize.rb
@@ -2,15 +2,15 @@ invoke_path = Dir.pwd
 
 review_folder = File.join(invoke_path, "review_#{Time.now.strftime('%F')}")
 
-`mkdir review_folder`
+`mkdir #{review_folder}`
 
 Dir.chdir(ARGV[0])
 
 foreign_branches = `git branch --list -r`
 
-foreign_branches.split("\n").each_with_index do |branch, index|
-  branch_folder = File.join(review_folder, index.to_s)
-  `mkdir branch_folder`
-  p branch.strip
-  # `git checkout #{branch}`
-end
+# foreign_branches.split("\n").each_with_index do |branch, index|
+#   branch_folder = File.join(review_folder, index.to_s)
+#   `mkdir branch_folder`
+#   p branch.strip
+#   # `git checkout #{branch}`
+# end
