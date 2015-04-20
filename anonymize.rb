586e03cfd20a81ecea398ba828ceea63ef965d11
Matthew Bunday <zenceph@gmail.com>
2015-04-20 15:30:46 -0400

    Making folders.

--- anonymize.rb
+++ anonymize.rb
@@ -1,10 +1,16 @@
 invoke_path = Dir.pwd
 
+review_folder = File.join(invoke_path, "review_#{Time.now.strftime('%F')}")
+
+`mkdir review_folder`
+
 Dir.chdir(ARGV[0])
 
 foreign_branches = `git branch --list -r`
 
 foreign_branches.split("\n").each_with_index do |branch, index|
+  branch_folder = File.join(review_folder, index.to_s)
+  `mkdir branch_folder`
   p branch.strip
-  puts `git checkout #{branch}`
+  # `git checkout #{branch}`
 end
